cde9dc103284f4affb8da0d72db51b928136f48f
jamescoll <james.evin.coll@gmail.com>
2014-05-29 20:03:16 +0100

    Added wiki pages and some unit tests

--- src/main/java/fileUtilities/test.java
+++ src/main/java/fileUtilities/test.java
@@ -1,6 +1,8 @@
 package fileUtilities;
 
 
+import java.util.HashSet;
+
 /**
  * Created by James on 28/05/2014.
  */
@@ -10,19 +12,17 @@ public class test {
     public static void main(String args[]) {
 
 
-        // FileExtensionFinder fExt = new FileExtensionFinder();
+        FileExtensionFinder fExt = new FileExtensionFinder();
 
-        //  HashSet<String> exts = fExt.getFileExtensions();
+        HashSet<String> exts = fExt.getFileExtensions();
 
-        //  for(String ext:exts) {
-        //      System.out.println(ext);
-        //  }
+        for (String ext : exts) {
+            System.out.println(ext);
+        }
 
         FileSystemScanner sfs = new FileSystemScanner();
         sfs.search();
 
 
-
-        ///blaarghhh
     }
 }
